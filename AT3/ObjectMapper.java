c53545375b4ba10278f8c33654c2d8a7c2ecef4d
lipe-menezes <99661869+lipe-menezes@users.noreply.github.com>
2024-06-17 22:31:43 -0300

    Add files via upload

---
+++ AT3/ObjectMapper.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.List;
+
+public class ObjectMapper {
+
+    public Livro readValue(String partes, Class<Livro> class1) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'readValue'");
+    }
+
+    public void writeValue(File file, List<Livro> livros) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'writeValue'");
+    }
+
+}
